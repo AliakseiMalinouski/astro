2f4f075db9e436eddbc9188bbec7393e634e2a76
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2025-09-25 18:08:15 +0200

    astro render mods

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), mdx()],
+  output: 'static',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
